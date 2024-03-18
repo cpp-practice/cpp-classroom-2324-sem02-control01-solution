3fa94007c5dedf263a015d45b359b8d59f72eeda
Koteika <dmitrii.nuzdin@gmail.com>
2024-03-18 03:28:08 +0300

    const_zip

--- src/task_1/zip.hpp
+++ src/task_1/zip.hpp
@@ -15,6 +15,11 @@ namespace control_01 {
             return std::get<N>(static_cast<std::tuple<Args&...>&>(*this));
         }
 
+        template <std::size_t N>
+        auto get() const {
+          return std::get<N>(static_cast<const std::tuple< Args&...>&>(*this));
+        }
+
         ZipTuple* operator->() {
             return this;
         }
@@ -25,6 +30,18 @@ namespace control_01 {
     };
 
 
+    namespace details{
+        template <typename... T>
+        struct is_const{
+            static constexpr bool value = (... || std::is_const_v<std::remove_reference_t< typename std::iterator_traits<T>::reference> >);
+        };
+
+        template <typename... T>
+        static constexpr bool is_const_v = is_const<T...>::value;  
+      
+    }
+
+
     template<typename... Iterators>
     class ZipIterator {
     public:
@@ -32,20 +49,28 @@ namespace control_01 {
 
         // using iterator_category = std::forward_iterator_tag; //zip_1
         using iterator_category = std::bidirectional_iterator_tag; //zip_2
-        using value_type = ZipTuple<typename std::iterator_traits<Iterators>::value_type...>;
-//    using value_type = ZipTuple<typename Iterators::value_type...>; <- doesn't work for int* :(
+
+        using value_type = std::conditional_t<
+            details::is_const_v<Iterators...>,
+            const ZipTuple<typename std::iterator_traits<Iterators>::value_type...>,
+            ZipTuple<typename std::iterator_traits<Iterators>::value_type...>
+            >;
+        //    using value_type = ZipTuple<typename Iterators::value_type...>; <-
+        //    doesn't work for int* :(
         using reference = value_type&;
         using pointer = value_type*;
         using difference_type = ptrdiff_t;
 
         // `reference` will return reference to the locally created object >:(
-        value_type operator*() {
+        value_type operator*() const {
             return std::apply([](auto&... iterators) {
                 return ZipTuple(*iterators...);
             }, iterators_tuple_);
         }
 
-        value_type operator->() {
+        
+
+        value_type operator->() const {
             return *(*this);
         }
 
@@ -111,7 +136,8 @@ namespace control_01 {
 //    begin  |
 //          end
 
-        using iterator = ZipIterator<typename Containers::iterator...>;
+        // using iterator = ZipIterator<typename Containers::iterator...>; //deosn't work for construcrot from const containers
+        using iterator = ZipIterator<decltype(std::declval<Containers>().begin())...>;
         using const_iterator = ZipIterator<typename Containers::const_iterator...>;
 
 //       reverse_iterators:
@@ -122,10 +148,12 @@ namespace control_01 {
 //           |   |
 //        rend   |
 //             rbegin
-//      iterating reverse_iterator != iterating container from common end
+//      iterating with reverse_iterator != iterating containers from common end() 
 //      we visit different set ov elemnts (compare visited range of elements on illustraion above)
 
-        using reverse_iterator = ZipIterator<typename Containers::reverse_iterator...>;
+        // using reverse_iterator = ZipIterator<typename Containers::reverse_iterator...>;
+        using reverse_iterator =
+            ZipIterator<decltype(std::declval<Containers>().rbegin())...>;
         using const_reverse_iterator = ZipIterator<typename Containers::const_reverse_iterator...>;
 
         iterator begin() {
@@ -134,7 +162,9 @@ namespace control_01 {
             }, containers_tuple_);
         }
 
-        const_iterator cbegin() {
+        const_iterator begin() const { return cbegin(); }
+
+        const_iterator cbegin() const{
             return std::apply([](const auto&... containers) {
                 return ZipIterator(containers.cbegin()...);
             }, containers_tuple_);
@@ -148,7 +178,9 @@ namespace control_01 {
               },
               containers_tuple_);
         }
-        const_reverse_iterator crbegin() {
+
+        const_reverse_iterator rbegin() const { return crbegin(); }
+        const_reverse_iterator crbegin() const {
           return std::apply(
               [](const auto&... containers) {
                 return ZipIterator(containers.crbegin()...);
@@ -170,12 +202,39 @@ namespace control_01 {
             }, containers_tuple_);
         
         }
+
+        const_iterator end() const { return cend(); }
+
+        const_iterator cend() const {
+          size_t size_ = size();
+
+          return std::apply(
+              [size_](const auto&... containers) {
+                return const_iterator(std::next(containers.cbegin(), size_)...);
+              },
+              containers_tuple_);
+        }
         reverse_iterator rend() {
           size_t size_ = size();
 
           return std::apply(
               [size_](auto&... containers) {
                 return reverse_iterator(std::next(containers.rbegin(), size_)...);
+              },
+              containers_tuple_);
+        }
+
+        const_reverse_iterator rend() const {
+          return crend();
+        }
+
+        const_reverse_iterator crend() const {
+          size_t size_ = size();
+
+          return std::apply(
+              [size_](const auto&... containers) {
+                return const_reverse_iterator(
+                    std::next(containers.rbegin(), size_)...);
               },
               containers_tuple_);
         }
